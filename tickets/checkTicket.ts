742fd89dbdd58aac812d9791e647d0b3f0a6680f
Gustavo Scharf <scharfgustavo@gmail.com>
2022-05-31 18:47:44 -0400

    checkTicket looks good now

--- tickets/checkTicket.ts
+++ tickets/checkTicket.ts
@@ -33,6 +33,33 @@ interface TicketInfo {
   coordinates?: Position[],
 }
 
+/**
+ * regex for description results in ugliness
+ * this trims away all the new lines and leaves one nice description string
+ * @param {string} description - locate description for locate
+ */
+function trimDescription(description: string): string {
+  description = description.slice(0, -2);
+  description = description.replaceAll("\n", " ");
+  description = description.replaceAll("'", "''");
+  return description;
+}
+
+/**
+ * turns "my name's gustavo" into
+ *       "my name''s gustavo"
+ * this way it can be escaped in postgresql
+ * @param {string} txt - arbritary string to escape single quote
+ */
+function escapeSingleQuote(txt: string): string {
+  return txt.replaceAll("'", "''");
+}
+
+/**
+ * page should be set to view a current ticket. it will go through the table of responses
+ * and return a list of Response objects
+ * @param {puppeteer.Page} page - page object 
+ */
 async function getResponses(page: puppeteer.Page): Promise<Response[]> {
   let responses = await page.evaluate(() => {
     const tableSelector = "body > app-root > div > desktop-root > div > mat-sidenav-container > mat-sidenav-content > div > ng-component > div.page-content > div:nth-child(3) > ticket-anon-simple-view > div > ticket-details-printing-text-and-service-areas > iq-view-list > div.iq-list-items";
@@ -63,22 +90,56 @@ async function getResponses(page: puppeteer.Page): Promise<Response[]> {
   return responses;
 }
 
+/**
+ * uses regex to parse the raw ticket text and get the pertinent information from each ticket
+ * @param {string} text - the full textContent from the ticketInfo element
+ */
 function parseTicketText(text: string): { ticket_number: string, city: string, street: string, cross_street: string, input_date: Date, expiration_date: Date, description: string, job_name: string, pl_number: number } {
+
+  let ticketNumberRegex = /Ticket : (\d{9})/;
+  let ticketNumberResult = text.match(ticketNumberRegex);
+
+  let streetRegex = /Street  : (.*)/;
+  let streetResult = text.match(streetRegex);
+
+  let crossStreetRegex = /Cross 1 : (.*)/;
+  let crossStreetResult = text.match(crossStreetRegex);
+
+  let callInDateRegex = /Taken: (\d{2}\/\d{2}\/\d{2})/;
+  let callInDateResult = text.match(callInDateRegex);
+
+  let expDateRegex = /Exp Date : (\d{2}\/\d{2}\/\d{2})/;
+  let expDateResult = text.match(expDateRegex);
+
+  let descriptionRegex = /Locat: ([\s\d\w\(\)\,\.\&\'\"\-\/]*):/;
+  let descriptionResult = text.match(descriptionRegex);
+
+  let cityRegex = /GeoPlace: (.*)/;
+  let cityResult = text.match(cityRegex);
+
+  let remarksRegex = /Remarks : (.*)/;
+  let remarksResult = text.match(remarksRegex);
+
   return {
-    ticket_number: 'test',
-    city: 'fdfj',
-    street: 'str',
-    cross_street: 'jifd',
-    input_date: new Date(),
-    expiration_date: new Date(),
-    description: 'jifd',
-    job_name: 'str',
-    pl_number: 1,
+    ticket_number: ticketNumberResult[1],
+    city: escapeSingleQuote(cityResult[1]),
+    street: escapeSingleQuote(streetResult[1]),
+    cross_street: escapeSingleQuote(crossStreetResult[1]),
+    input_date: new Date(callInDateResult[1]),
+    expiration_date: new Date(expDateResult[1]),
+    description: trimDescription(descriptionResult[1]),
+    job_name: remarksResult[1].split(" ")[0],
+    pl_number: Number(remarksResult[1].slice(-2)),
   }
 }
 
 
-async function getTicketInfo(ticketNumber: string): Promise<TicketInfo> {
+/**
+ * basically the main function of this file, it takes a ticket number and
+ * parses all the ticket text as well as gets the responses. 
+ * @param {string} ticketNumber - needs to be a string because it could end in 0 
+ */
+export async function getTicketInfo(ticketNumber: string): Promise<TicketInfo> {
   const browser = await puppeteer.launch({
     headless: false,
   });
@@ -104,15 +165,7 @@ async function getTicketInfo(ticketNumber: string): Promise<TicketInfo> {
   await page.waitForSelector(ticketTextSelector);
   let ticketText = await page.$eval(ticketTextSelector, el => el.textContent);
   let parsedText = parseTicketText(ticketText);
-
   const responses = await getResponses(page);
-  console.log(responses);
-
-
-
-  setTimeout((br) => {
-    browser.close();
-  }, 1000000, browser)
 
   return {
     ticket_number: parsedText.ticket_number,
@@ -122,10 +175,8 @@ async function getTicketInfo(ticketNumber: string): Promise<TicketInfo> {
     input_date: parsedText.input_date,
     expiration_date: parsedText.expiration_date,
     job_name: parsedText.job_name,
-    responses: [],
+    responses: responses,
     description: parsedText.description,
     pl_number: parsedText.pl_number,
   }
 }
-
-getTicketInfo('143204279');
