812916048c8a447cbfc3beda6b3539795f8021ee
Gustavo Scharf <scharfgustavo@gmail.com>
2022-06-01 02:34:47 -0400

    lot of stuff. there's a viewJob route which has the skeleton set up for inputting tickets. which is the next step.. time to fill in the info in the boxes with data from the database

---
+++ output/public/scripts/viewJob.js
@@ -0,0 +1,21 @@
+let map = L.map('map').setView([28.54, -81.391], 8);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'BLUEOCEAN',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ3NjaGFyZjk0IiwiYSI6ImNreWd2am9mODBjbnMyb29sNjZ2Mnd1OW4ifQ.1cSadM_VR54gigTAsVVGng'
+}).addTo(map);
+function toggleTicketPopup() {
+    let popup = document.getElementById('addTicketPopup');
+    popup.classList.toggle("show");
+}
+/**
+ * displays the popup and fills it with information about specific ticket
+ * after calling api to get server to check ticket
+ * after this, get the user to draw the ticket on the map
+ */
+function addTicket() {
+    toggleTicketPopup();
+}
